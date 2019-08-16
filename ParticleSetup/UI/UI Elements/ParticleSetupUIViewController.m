387b525197d03cda409e06efb4e9f59e2dd35912
Raimundas Sakalauskas <raimis@particle.io>
2019-08-16 11:30:16 +0300

    force setup views to display in light style on ios13

--- ParticleSetup/UI/UI Elements/ParticleSetupUIViewController.m
+++ ParticleSetup/UI/UI Elements/ParticleSetupUIViewController.m
@@ -23,7 +23,16 @@
 
 -(void)viewDidLoad {
     [super viewDidLoad];
-    
+
+
+    //Force light mode on iOS 13
+    if (@available(iOS 13.0, *)) {
+        if ([self respondsToSelector:NSSelectorFromString(@"overrideUserInterfaceStyle")])
+        {
+            [self setValue:@(UIUserInterfaceStyleLight) forKey:@"overrideUserInterfaceStyle"];
+        }
+    }
+
     //    self.view.backgroundColor = [ParticleSetupCustomization sharedInstance].pageBackgroundColor;
     if ([ParticleSetupCustomization sharedInstance].pageBackgroundImage)
     {
