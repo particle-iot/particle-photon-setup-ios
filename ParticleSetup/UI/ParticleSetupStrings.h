956c4cc25de3acf548ec11b70a672b5f842d2f17
Raimundas Sakalauskas <raimis@particle.io>
2019-10-23 17:45:10 +0300

    add generated files (necessary for them to get shipped with the pod)

---
+++ ParticleSetup/UI/ParticleSetupStrings.h
@@ -0,0 +1,13 @@
+// Generated on 2019-10-23 17:38:31 by iOS.py
+
+#define ParticleSetupStrings_Action_Cancel = NSLocalizedStringFromTable(@"ParticleSetupStrings.Action.Cancel", @"ParticleSetupStrings", @"")
+#define ParticleSetupStrings_Action_LogOut = NSLocalizedStringFromTable(@"ParticleSetupStrings.Action.LogOut", @"ParticleSetupStrings", @"")
+
+#define ParticleSetupStrings_GetReady_Button_Ready = NSLocalizedStringFromTable(@"ParticleSetupStrings.GetReady.Button.Ready", @"ParticleSetupStrings", @"")
+
+#define ParticleSetupStrings_GetReady_Text1 = NSLocalizedStringFromTable(@"ParticleSetupStrings.GetReady.Text1", @"ParticleSetupStrings", @"")
+#define ParticleSetupStrings_GetReady_Text2 = NSLocalizedStringFromTable(@"ParticleSetupStrings.GetReady.Text2", @"ParticleSetupStrings", @"")
+#define ParticleSetupStrings_GetReady_Text3 = NSLocalizedStringFromTable(@"ParticleSetupStrings.GetReady.Text3", @"ParticleSetupStrings", @"")
+
+
+
