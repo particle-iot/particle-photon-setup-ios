33e3f8b2b83c01634560f8138e90583d1eba8cc6
Raimundas Sakalauskas <raimis@particle.io>
2019-10-25 01:38:09 +0300

    make variable replacement part of NSString extension

--- ParticleSetup/UI/ParticleSetupStringsExtensions.m
+++ ParticleSetup/UI/ParticleSetupStringsExtensions.m
@@ -9,7 +9,25 @@
 @implementation NSString (ParticleString)
 
 - (NSString * _Nullable)particleLocalized {
-    return NSLocalizedStringFromTableInBundle(self, @"ParticleSetupStrings", [ParticleSetupMainController getResourcesBundle], @"");
+
+    if ([self isEqualToString:@"ParticleSetupStrings.SelectNetwork.Button.NotListed"]) {
+        NSLog(@"[NSString replaceVariablesInStrings: NSLocalizedStringFromTableInBundle(self, @\"ParticleSetupStrings\", [ParticleSetupMainController getResourcesBundle], @"")]; = %@", [NSString replaceVariablesInStrings: NSLocalizedStringFromTableInBundle(self, @"ParticleSetupStrings", [ParticleSetupMainController getResourcesBundle], @"")]);
+    }
+
+    return [NSString replaceVariablesInStrings: NSLocalizedStringFromTableInBundle(self, @"ParticleSetupStrings", [ParticleSetupMainController getResourcesBundle], @"")];
+
+}
+
+
++ (NSString *)replaceVariablesInStrings:(NSString *)value {
+    value = [value stringByReplacingOccurrencesOfString:@"{device}" withString:[ParticleSetupCustomization sharedInstance].deviceName];
+    value = [value stringByReplacingOccurrencesOfString:@"{brand}" withString:[ParticleSetupCustomization sharedInstance].brandName];
+    value = [value stringByReplacingOccurrencesOfString:@"{color}" withString:[ParticleSetupCustomization sharedInstance].listenModeLEDColorName];
+    value = [value stringByReplacingOccurrencesOfString:@"{mode button}" withString:[ParticleSetupCustomization sharedInstance].modeButtonName];
+    value = [value stringByReplacingOccurrencesOfString:@"{network prefix}" withString:[ParticleSetupCustomization sharedInstance].networkNamePrefix];
+    value = [value stringByReplacingOccurrencesOfString:@"{product}" withString:[ParticleSetupCustomization sharedInstance].productName];
+
+    return value;
 }
 
 @end
